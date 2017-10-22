9290bffc4cc81d49841af24300524fa93e0d9798
Wolfgang S <32031099+eleschrott@users.noreply.github.com>
2017-10-22 09:21:41 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,28 +5,28 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-#define PWM_INPUT		PB4		// PWM low frequency input pin
-#define PWM_OUTPUT		PB0		// PWM high frequency output pin
+#define PWM_INPUT	PB4	// PWM low frequency input pin
+#define PWM_OUTPU	PB0	// PWM high frequency output pin
 
-#define PWM_FREQ_HIGH	1		// PWM frequency:	0 = 16KHz
-								//					1 = 32KHz 
+#define PWM_FREQ_HIGH	1	// PWM frequency:	0 = 16KHz
+				//			1 = 32KHz 
 
-#define DUTY_MIN_LIMIT	2		// min/max limits according...
-#define DUTY_MAX_LIMIT	249		// ... your fan
+#define DUTY_MIN_LIMIT	2	// min/max limits according...
+#define DUTY_MAX_LIMIT	249	// ... your fan
 
 #define SAMPLE_RATE     1792  	// samples of input pulses to calculate the duty rate
-#define SAMPLE_TIME		524288 	// max. sample time to reset timer1 overflow
+#define SAMPLE_TIME	524288 	// max. sample time to reset timer1 overflow
 
 volatile uint32_t	pulseOn = 0,
-					pulseTime = 0,
-					pulseLength = 0,
-					pwmValue = 0,
-					samples = 0;
+			pulseTime = 0,
+			pulseLength = 0,
+			pwmValue = 0,
+			samples = 0;
 volatile uint8_t	pwmDuty = 0,
-					timerVal = 0;
+			timerVal = 0;
 volatile bool		fallingEdge = false,
-					pwmOn = true,
-					inputPulse = false;
+			pwmOn = true,
+			inputPulse = false;
 				
 int main()
 {
@@ -34,20 +34,20 @@ int main()
 	PORTB &= ~_BV(PWM_OUTPUT);	// set PWM_PIN to LOW
 
 	PCMSK |= _BV(PWM_INPUT);	// turn on interrupts on pin PWM_INPUT
-	GIMSK |= _BV(PCIE);			// turns on pin change interrupts
+	GIMSK |= _BV(PCIE);		// turns on pin change interrupts
 
-	TCCR1 |= _BV(CS01);			// timer1 prescaler 8 = 1MHz 
+	TCCR1 |= _BV(CS01);		// timer1 prescaler 8 = 1MHz 
 	TIMSK |= _BV(TOIE1);		// set overflow interrupt on timer1
 
-	sei();						// enable interrupts
+	sei();				// enable interrupts
 
 	#if defined(PWM_FREQ_HIGH) && PWM_FREQ_HIGH == 1 // if PWM_FREQ_HIGH set to 1, use fast pwm mode
 	TCCR0A |= _BV(COM0A1) | _BV(WGM01) | _BV(WGM00); // set OC0A on compare match and fast pwm mode with 32KHz
 	#else
-	TCCR0A |= _BV(COM0A1) | _BV(WGM00);				 // set OC0A on compare match and normal pwm with 16KHz
+	TCCR0A |= _BV(COM0A1) | _BV(WGM00);		 // set OC0A on compare match and normal pwm with 16KHz
 	#endif
 
-	TCCR0B |= _BV(CS00);							 // timer0 no prescaling = 8MHz
+	TCCR0B |= _BV(CS00);				 // timer0 no prescaling = 8MHz
 
 	while (1)
 	{
@@ -62,9 +62,9 @@ int main()
 		else if (((pwmOn == true) && (pwmDuty <= DUTY_MIN_LIMIT)) || ((pwmOn == true) && (inputPulse == false))) // stop pwm
 		{
 			cli();
-			TCCR0B	&= ~_BV(CS00);		 // stop timer0 
-			PORTB	&= ~_BV(PWM_OUTPUT); // set PWM_OUTPUT to LOW
-			DDRB	&= ~_BV(PWM_OUTPUT); // erase PWM_PIN from output register
+			TCCR0B	&= ~_BV(CS00);		// stop timer0 
+			PORTB	&= ~_BV(PWM_OUTPUT); 	// set PWM_OUTPUT to LOW
+			DDRB	&= ~_BV(PWM_OUTPUT); 	// erase PWM_PIN from output register
 			pwmOn = false;
 			pwmValue = 0;
 			samples = 0;
@@ -129,7 +129,7 @@ ISR(PCINT0_vect)
 				inputPulse = true;
 				pwmValue = 0;
 				samples = 0;
-				OCR0A = pwmDuty; // set duty to the register
+				OCR0A = pwmDuty; // set duty to register
 			}
 			return;
 		}
