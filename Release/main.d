c4c466fde8a47e6c12e5c32e65bcc1c2295db17a
Wolfgang S <32031099+eleschrott@users.noreply.github.com>
2017-10-24 19:21:32 +0200

    Add files via upload

--- Release/main.d
+++ Release/main.d
@@ -5,8 +5,8 @@ main.d main.o: .././main.c \
  d:\programme\elektronik\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
  d:\programme\elektronik\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
  d:\programme\elektronik\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
- D:\Programme\Elektronik\Atmel\Studio\7.0\Packs\Atmel\ATtiny_DFP\1.3.132\include/avr/iotn85.h \
- D:\Programme\Elektronik\Atmel\Studio\7.0\Packs\Atmel\ATtiny_DFP\1.3.132\include/avr/iotnx5.h \
+ D:\Programme\Elektronik\Atmel\Studio\7.0\Packs\atmel\ATtiny_DFP\1.3.147\include/avr/iotn85.h \
+ D:\Programme\Elektronik\Atmel\Studio\7.0\Packs\atmel\ATtiny_DFP\1.3.147\include/avr/iotnx5.h \
  d:\programme\elektronik\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
  d:\programme\elektronik\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
  d:\programme\elektronik\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
@@ -26,9 +26,9 @@ d:\programme\elektronik\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\i
 
 d:\programme\elektronik\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
 
-D:\Programme\Elektronik\Atmel\Studio\7.0\Packs\Atmel\ATtiny_DFP\1.3.132\include/avr/iotn85.h:
+D:\Programme\Elektronik\Atmel\Studio\7.0\Packs\atmel\ATtiny_DFP\1.3.147\include/avr/iotn85.h:
 
-D:\Programme\Elektronik\Atmel\Studio\7.0\Packs\Atmel\ATtiny_DFP\1.3.132\include/avr/iotnx5.h:
+D:\Programme\Elektronik\Atmel\Studio\7.0\Packs\atmel\ATtiny_DFP\1.3.147\include/avr/iotnx5.h:
 
 d:\programme\elektronik\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
 
